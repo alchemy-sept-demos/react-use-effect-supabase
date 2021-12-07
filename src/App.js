a9a3d373f228835da45c09c1fe90f87c8090a83c
Julie Nisbet <jmnisbet@gmail.com>
2021-12-06 16:12:02 -0800

    adding missing demo code

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import { getRestaurants } from './services/restaurants';
 
 function App() {
   const [restaurants, setRestaurants] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await getRestaurants();
       console.log(data);
+      setRestaurants(data);
     };
     fetchData();
   }, []);
   return (
     <div className="App">
       <h1>Restaurant List</h1>
+      {restaurants.map((r) => (
+        <p key={r.id}>{r.name}</p>
+      ))}
     </div>
   );
 }
